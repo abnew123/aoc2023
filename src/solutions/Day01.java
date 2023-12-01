2f2ae78cb3ea7537b07fa31d56e6f60aac8de4f7
abnew123 <shadowghosts31415@gmail.com>
2023-12-02 01:27:18 +1100

    Day 1

--- src/solutions/Day01.java
+++ src/solutions/Day01.java
@@ -1,9 +1,11 @@
 package src.solutions;
 
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import src.meta.DayTemplate;
 
-public class Day01 {
+import java.util.*;
+import java.io.*;
+
+public class Day01 extends DayTemplate {
 
     /**
      * Main solving method.
@@ -17,11 +19,39 @@ public class Day01 {
      * @throws FileNotFoundException
      */
     public String solve(boolean part1, Scanner in) throws FileNotFoundException{
-        if(part1){
-            return "hi";
+        int answer = 0;
+        List<String> lines = new ArrayList<>();
+        while(in.hasNext()){
+            lines.add(in.nextLine());
         }
-        else{
-            return "bye";
+        for(String line: lines){
+            answer+= 10 * helper(line, true, part1);
+            answer+= helper(line, false, part1);
         }
+        return answer+"";
+    }
+
+    private int helper(String line, boolean forwards, boolean part1){
+        String[] digits = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        char[] chars = line.toCharArray();
+        for(int i= 0; i < chars.length; i++){
+            int index = forwards?i:(chars.length - i - 1);
+            if(!part1){
+                for(int j = 0; j < digits.length; j++){
+                    if(index + digits[j].length() > line.length()){
+                        continue;
+                    }
+                    String substring = line.substring(index, index + digits[j].length());
+                    if(substring.contains(digits[j])){
+                        return j + 1;
+                    }
+                }
+            }
+            char ch = chars[index];
+            if(Character.isDigit(ch)){
+                return Character.digit(ch, 10);
+            }
+        }
+        return -1;
     }
 }
