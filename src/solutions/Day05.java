2710da9c473a96ca28f3854567d19ef67b463508
abnew123 <shadowghosts31415@gmail.com>
2023-12-05 16:25:13 +1100

    Day 5

---
+++ src/solutions/Day05.java
@@ -0,0 +1,118 @@
+package src.solutions;
+
+import src.meta.DayTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day05 extends DayTemplate {
+
+    /**
+     * Main solving method.
+     *
+     * @param part1 The solver will solve part 1 if param is set to true.
+     *              The solver will solve part 2 if param is set to false.
+     * @param in    The solver will read data from this Scanner.
+     * @return Returns answer in string format.
+     */
+    public String solve(boolean part1, Scanner in) {
+        long answer = 10000000000L;
+        List<RangeMap> rangeMaps = new ArrayList<>();
+        List<Range> tmp = new ArrayList<>();
+        String[] stringSeeds = in.nextLine().split(" ");
+        long[] seeds = new long[stringSeeds.length - 1];
+        for (int i = 1; i < stringSeeds.length; i++) {
+            seeds[i - 1] = Long.parseLong(stringSeeds[i]);
+        }
+        while (in.hasNext()) {
+            String line = in.nextLine();
+            if (line.equals("")) {
+                continue;
+            }
+            if (line.contains("map")) {
+                if (tmp.size() > 0) {
+                    rangeMaps.add(new RangeMap(tmp));
+                }
+                tmp = new ArrayList<>();
+            } else {
+                tmp.add(new Range(line));
+            }
+        }
+        rangeMaps.add(new RangeMap(tmp));
+        if (part1) {
+            for (Long seed : seeds) {
+                long val = seed;
+                for (RangeMap rangeMap : rangeMaps) {
+                    val = rangeMap.convert(val);
+                }
+                if (val < answer) {
+                    answer = val;
+                }
+            }
+        } else {
+            for(int i = 0; i < seeds.length; i+=2){
+                System.out.println("check");
+                for(long j = seeds[i]; j < seeds[i] + seeds[i + 1]; j++){
+                    long val = j;
+                    for (RangeMap rangeMap : rangeMaps) {
+                        val = rangeMap.convert(val);
+                    }
+                    if (val < answer) {
+                        answer = val;
+                        System.out.println(answer);
+                    }
+                }
+            }
+        }
+
+
+        return answer + "";
+    }
+}
+
+class Range {
+    long destination;
+    long source;
+    long range;
+
+    public Range(long des, long src, long r) {
+        destination = des;
+        source = src;
+        range = r;
+    }
+
+    public Range(String line) {
+        String[] pieces = line.split(" ");
+        destination = Long.parseLong(pieces[0]);
+        source = Long.parseLong(pieces[1]);
+        range = Long.parseLong(pieces[2]);
+    }
+}
+
+class RangeMap {
+    List<Long> starts;
+    List<Long> ends;
+    List<Long> betweens;
+
+    public RangeMap(List<Range> ranges) {
+        starts = new ArrayList<>();
+        ends = new ArrayList<>();
+        betweens = new ArrayList<>();
+        for (Range range : ranges) {
+            starts.add(range.source);
+            ends.add(range.destination);
+            betweens.add(range.range);
+        }
+    }
+
+    public long convert(long val) {
+        for (int i = 0; i < starts.size(); i++) {
+            if (starts.get(i) <= val && starts.get(i) + betweens.get(i) > val) {
+                return ends.get(i) + (val - starts.get(i));
+            }
+        }
+
+        return val;
+    }
+}
