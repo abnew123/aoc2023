550283f85342a56be4a84eb499fc254feb05a4de
abnew123 <shadowghosts31415@gmail.com>
2023-12-18 12:15:21 +1100

    10s to 0,3s Day 17

--- src/solutions/Day17.java
+++ src/solutions/Day17.java
@@ -7,11 +7,9 @@ import java.util.*;
 public class Day17 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
-        long answer = 1000000000;
         List<String[]> tmp = new ArrayList<>();
         while (in.hasNext()) {
-            String[] line = in.nextLine().split("");
-            tmp.add(line);
+            tmp.add(in.nextLine().split(""));
         }
         int[][] grid = new int[tmp.get(0).length][tmp.size()];
         for (int i = 0; i < grid.length; i++) {
@@ -19,82 +17,54 @@ public class Day17 extends DayTemplate {
                 grid[i][j] = Integer.parseInt(tmp.get(j)[i]);
             }
         }
-        if (part1) {
-            Queue<Location> queue = new PriorityQueue<>();
-            Set<Location> allSeen = new HashSet<>();
-            queue.add(new Location(0, 0, 0, 0, 0));
-            int[] xs = new int[]{-1, 0, 0, 1};
-            int[] ys = new int[]{0, -1, 1, 0};
-            while (queue.size() > 0) {
-                Location c = queue.poll();
-                allSeen.add(c);
-                if (c.x == grid.length - 1 && c.y == grid[0].length - 1) {
-                    return c.currBest + "";
-                }
-                for (int i = 1; i < 5; i++) {
-                    if (i + c.direction == 5) {
-                        continue;
-                    }
-                    int newx = c.x + xs[i - 1];
-                    int newy = c.y + ys[i - 1];
-                    if (newx >= 0 && newy >= 0 && newx < grid.length && newy < grid[0].length) {
-                        int steps = 1;
-                        if (c.direction == i) {
-                            if (c.steps >= 3) {
-                                continue;
-                            }
-                            steps = c.steps + 1;
-                        }
-                        Location tmp2 = new Location(newx, newy, i, steps, c.currBest + grid[newx][newy]);
-                        if (!allSeen.contains(tmp2) && !queue.contains(tmp2)) {
-                            queue.add(tmp2);
-                        }
-                    }
-                }
-            }
-        }
-        else{
-            Queue<Location> queue = new PriorityQueue<>();
-            Set<Location> allSeen = new HashSet<>();
-            queue.add(new Location(0, 0, 0, 0, 0));
-            int[] xs = new int[]{-1, 0, 0, 1};
-            int[] ys = new int[]{0, -1, 1, 0};
-            while (queue.size() > 0) {
-                Location c = queue.poll();
-                allSeen.add(c);
-                if (c.x == grid.length - 1 && c.y == grid[0].length - 1 && c.steps >=4) {
-                    return c.currBest + "";
-                }
-                for (int i = 1; i < 5; i++) {
-                    if (i + c.direction == 5) {
-                        continue;
-                    }
-                    int newx = c.x + xs[i - 1];
-                    int newy = c.y + ys[i - 1];
-                    if (newx >= 0 && newy >= 0 && newx < grid.length && newy < grid[0].length) {
-                        int steps = 1;
-                        if (c.direction == i) {
-                            if (c.steps >= 10) {
-                                continue;
-                            }
-                            steps = c.steps + 1;
-                        }
-                        else{
-                            if(c.steps < 4 && c.direction != 0){
-                                continue;
-                            }
-                        }
-                        Location tmp2 = new Location(newx, newy, i, steps, c.currBest + grid[newx][newy]);
-                        if (!allSeen.contains(tmp2) && !queue.contains(tmp2)) {
-                            queue.add(tmp2);
-                        }
-                    }
-                }
-            }
-        }
-        return answer + "";
+        return helper(grid, part1?1:4, part1?3:10) + "";
     }
 
+    private long helper(int[][] grid, int min, int max){
+        int[][][] debug = new int[grid.length][grid[0].length][5];
+        for(int i = 0; i < debug.length; i++){
+            for(int j = 0; j < debug[0].length; j++){
+                for(int k = 0; k < debug[0][0].length; k++){
+                    debug[i][j][k] = 999999;
+                }
+            }
+        }
+        long answer = 999999;
+        Queue<Location> queue = new PriorityQueue<>();
+        Set<Location> allSeen = new HashSet<>();
+        queue.add(new Location(0, 0, 0, 0));
+        int[] xs = new int[]{-1, 0, 0, 1};
+        int[] ys = new int[]{0, -1, 1, 0};
+        while (queue.size() > 0) {
+            Location c = queue.poll();
+            allSeen.add(c);
+            if (c.x == grid.length - 1 && c.y == grid[0].length - 1) {
+                answer = Math.min(answer, c.currBest);
+            }
+            for (int i = 1; i < 5; i++) {
+                if (i + c.direction == 5 || i == c.direction) {
+                    continue;
+                }
+                int path = c.currBest;
+                for(int j = 1; j <= max; j++){
+                    int newx = c.x + j * xs[i - 1];
+                    int newy = c.y + j * ys[i - 1];
+                    if (newx < 0 || newy < 0 || newx >= grid.length || newy >= grid[0].length) {
+                        break;
+                    }
+                    path+= grid[newx][newy];
+                    if(j >= min && path < debug[newx][newy][i]){
+                        debug[newx][newy][i] = path;
+                        Location tmp2 = new Location(newx, newy, i,  path);
+                        if (!allSeen.contains(tmp2)) {
+                            queue.add(tmp2);
+                        }
+                    }
+                }
+            }
+        }
+        return answer;
+    }
 
 }
 
@@ -102,14 +72,12 @@ class Location implements Comparable<Location> {
     int x;
     int y;
     int direction;
-    int steps;
     int currBest;
 
-    public Location(int x, int y, int direction, int steps, int currBest) {
+    public Location(int x, int y, int direction, int currBest) {
         this.x = x;
         this.y = y;
         this.direction = direction;
-        this.steps = steps;
         this.currBest = currBest;
     }
 
@@ -119,18 +87,13 @@ class Location implements Comparable<Location> {
     }
 
     @Override
-    public String toString() {
-        return x + " " + y + " " + direction + " " + steps + " " + currBest;
-    }
-
-    @Override
     public boolean equals(Object other) {
         Location o = (Location) other;
-        return o.x == x && o.y == y && o.direction == direction && o.steps == steps;
+        return o.x == x && o.y == y && o.direction == direction;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(x, y, direction, steps);
+        return Objects.hash(x, y, direction);
     }
 }
