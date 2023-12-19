9eedab1288923b3e00fac6209b565a4435c80683
abnew123 <shadowghosts31415@gmail.com>
2023-12-19 17:44:07 +1100

    cleanup Day 19

--- src/solutions/Day19.java
+++ src/solutions/Day19.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 public class Day19 extends DayTemplate {
 
+    public static String[] allCategories = new String[]{"x", "m", "a", "s"};
+
     /**
      * Main solving method.
      *
@@ -28,7 +30,6 @@ public class Day19 extends DayTemplate {
                     String[] pieces = line.split("[{}]");
                     workflows.put(pieces[0], new Workflow(pieces[1]));
                 } else {
-
                     parts.add(new Part(line));
                 }
             }
@@ -39,17 +40,17 @@ public class Day19 extends DayTemplate {
             Workflow w = workflows.get("in");
             w.getValid(accepted, workflows, tuple);
             for (SubRangeTuple t : accepted) {
-                long xRange = t.upper.get("x") - t.lower.get("x") + 1;
-                long mRange = t.upper.get("m") - t.lower.get("m") + 1;
-                long aRange = t.upper.get("a") - t.lower.get("a") + 1;
-                long sRange = t.upper.get("s") - t.lower.get("s") + 1;
-                answer += xRange * mRange * aRange * sRange;
+                long rangeTotal = 1;
+                for (String s : allCategories) {
+                    rangeTotal *= t.upper.get(s) - t.lower.get(s) + 1;
+                }
+                answer += rangeTotal;
             }
         } else {
             List<Part> accepted = new ArrayList<>();
             for (Part p : parts) {
                 Workflow w = workflows.get("in");
-                String result = "";
+                String result;
                 while (true) {
                     result = w.process(p);
                     if (result.equals("A")) {
@@ -62,12 +63,8 @@ public class Day19 extends DayTemplate {
                     w = workflows.get(result);
                 }
             }
-
             for (Part p : accepted) {
-                answer += p.vals.get("x");
-                answer += p.vals.get("m");
-                answer += p.vals.get("a");
-                answer += p.vals.get("s");
+                answer += p.vals.get("x") + p.vals.get("m") + p.vals.get("a") + p.vals.get("s");
             }
         }
         return answer + "";
@@ -88,7 +85,7 @@ class Workflow {
     }
 
     public String process(Part p) {
-        String ret = "";
+        String ret;
         for (Condition c : conditions) {
             ret = c.process(p);
             if (ret != null) {
@@ -100,8 +97,6 @@ class Workflow {
 
     public void getValid(List<SubRangeTuple> accepted, Map<String, Workflow> workflows, SubRangeTuple tuple) {
         for (Condition c : conditions) {
-            System.out.println("analyzing condition: " + c);
-            System.out.println("with solution space: " + tuple);
             tuple = c.addValid(accepted, workflows, tuple);
         }
         if (fin.equals("A") || fin.equals("R")) {
@@ -117,17 +112,14 @@ class Workflow {
 class Condition {
     String var;
     boolean greater;
-    String ifYes;
     int threshold;
-
-    String form;
+    String ifYes;
 
     public Condition(String l) {
         greater = l.contains(">");
         var = l.split("[<>:]")[0];
-        ifYes = l.split("[<>:]")[2];
         threshold = Integer.parseInt(l.split("[<>:]")[1]);
-        form = l;
+        ifYes = l.split("[<>:]")[2];
     }
 
     public String process(Part p) {
@@ -142,8 +134,6 @@ class Condition {
     public SubRangeTuple addValid(List<SubRangeTuple> accepted, Map<String, Workflow> workflows, SubRangeTuple tuple) {
         SubRangeTuple newTuple = new SubRangeTuple(tuple);
         SubRangeTuple currTuple = new SubRangeTuple(tuple);
-        System.out.println("current processing condition: " + form);
-        System.out.println("processing workflow " + ifYes + " next");
         if (greater) {
             if (currTuple.upper.get(var) <= threshold) {
                 return newTuple;
@@ -153,15 +143,6 @@ class Condition {
             if (newTuple.lower.get(var) >= threshold) {
                 newTuple.upper.put(var, newTuple.lower.get(var));
             }
-            if (ifYes.equals("A") || ifYes.equals("R")) {
-                if (ifYes.equals("A")) {
-                    accepted.add(currTuple);
-                }
-            } else {
-                Workflow w = workflows.get(ifYes);
-                w.getValid(accepted, workflows, currTuple);
-            }
-
         } else {
             if (currTuple.lower.get(var) >= threshold) {
                 return newTuple;
@@ -171,20 +152,16 @@ class Condition {
             if (newTuple.upper.get(var) <= threshold) {
                 newTuple.upper.put(var, newTuple.lower.get(var));
             }
-            if (ifYes.equals("A") || ifYes.equals("R")) {
-                if (ifYes.equals("A")) {
-                    accepted.add(currTuple);
-                }
-            } else {
-                Workflow w = workflows.get(ifYes);
-                w.getValid(accepted, workflows, currTuple);
+        }
+        if (ifYes.equals("A") || ifYes.equals("R")) {
+            if (ifYes.equals("A")) {
+                accepted.add(currTuple);
             }
+        } else {
+            Workflow w = workflows.get(ifYes);
+            w.getValid(accepted, workflows, currTuple);
         }
         return newTuple;
-    }
-
-    public String toString() {
-        return form;
     }
 }
 
@@ -192,12 +169,8 @@ class Part {
     Map<String, Integer> vals;
 
     public Part(String line) {
-        vals = new HashMap<>();
         String[] pieces = line.split("[{}=,xmas]");
-        vals.put("x", Integer.parseInt(pieces[3]));
-        vals.put("m", Integer.parseInt(pieces[6]));
-        vals.put("a", Integer.parseInt(pieces[9]));
-        vals.put("s", Integer.parseInt(pieces[12]));
+        vals = Map.of("x", Integer.parseInt(pieces[3]), "m", Integer.parseInt(pieces[6]), "a", Integer.parseInt(pieces[9]), "s", Integer.parseInt(pieces[12]));
     }
 }
 
@@ -206,29 +179,14 @@ class SubRangeTuple {
     Map<String, Integer> lower = new HashMap<>();
 
     public SubRangeTuple() {
-        upper.put("x", 4000);
-        upper.put("m", 4000);
-        upper.put("a", 4000);
-        upper.put("s", 4000);
-        lower.put("x", 1);
-        lower.put("m", 1);
-        lower.put("a", 1);
-        lower.put("s", 1);
+        for (String s : Day19.allCategories) {
+            upper.put(s, 4000);
+            lower.put(s, 1);
+        }
     }
 
     public SubRangeTuple(SubRangeTuple tuple) {
         upper.putAll(tuple.upper);
         lower.putAll(tuple.lower);
-    }
-
-    @Override
-    public String toString() {
-        String ret = "";
-        ret += "x: " + lower.get("x") + "  - " + upper.get("x") + "\n";
-        ret += "m: " + lower.get("m") + "  - " + upper.get("m") + "\n";
-        ret += "a: " + lower.get("a") + "  - " + upper.get("a") + "\n";
-        ret += "s: " + lower.get("s") + "  - " + upper.get("s") + "\n";
-
-        return ret;
     }
 }
