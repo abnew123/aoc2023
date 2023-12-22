c7987cfee33767505836008babd154c9a910389c
abnew123 <shadowghosts31415@gmail.com>
2023-12-21 23:01:20 -0800

    cleaner code and 10-20x faster part 2

--- src/solutions/Day22.java
+++ src/solutions/Day22.java
@@ -15,15 +15,13 @@ public class Day22 extends DayTemplate {
         int[] ys = new int[]{0,1,0};
         int[] zs = new int[]{0,0,1};
         List<Brick> bricks = new ArrayList<>();
-        int index = 0;
         while (in.hasNext()) {
             String line = in.nextLine();
-            Brick b = new Brick(line, index);
+            Brick b = new Brick(line);
             bricks.add(b);
             maxX = Math.max(maxX, b.x + b.size);
             maxY = Math.max(maxY, b.y + b.size);
             maxZ = Math.max(maxZ, b.z + b.size);
-            index++;
         }
         grid = new int[maxX][maxY][maxZ];
         for(int i = 0 ; i < grid.length; i++){
@@ -32,6 +30,10 @@ public class Day22 extends DayTemplate {
                     grid[i][j][k] = -1;
                 }
             }
+        }
+        bricks.sort(null);
+        for(int i = 0; i < bricks.size(); i++){
+            bricks.get(i).id = i;
         }
         for(Brick b: bricks){
             for(int i = 0; i < b.size; i++){
@@ -61,34 +63,30 @@ public class Day22 extends DayTemplate {
                             grid[b.x + k * xs[b.dir]][b.y + k * ys[b.dir]][b.z + k * zs[b.dir] - 1] = b.id;
                             grid[b.x + k * xs[b.dir]][b.y + k * ys[b.dir]][b.z + k * zs[b.dir]] = -1;
                         }
-                        b.setZ(b.z - 1);
+                        b.z = b.z - 1;
                         somethingMoved = true;
                     }
                 }
             }
         }
-
         for(Brick b: bricks){
             if(b.dir != 2){
                 for(int k = 0; k < b.size; k++){
                     if(grid[b.x + k * xs[b.dir]][b.y + k * ys[b.dir]][b.z + 1] != -1){
-                        Brick b2 = bricks.get(grid[b.x + k * xs[b.dir]][b.y + k * ys[b.dir]][b.z  + 1]);
-                        b2.dependencies.add(b.id);
+                        bricks.get(grid[b.x + k * xs[b.dir]][b.y + k * ys[b.dir]][b.z  + 1]).dependencies.add(b.id);
                     }
                 }
             }
             else{
                 if(grid[b.x][b.y][b.z + b.size] != -1){
-                    Brick b2 = bricks.get(grid[b.x][b.y][b.z + b.size]);
-                    b2.dependencies.add(b.id);
+                    bricks.get(grid[b.x][b.y][b.z + b.size]).dependencies.add(b.id);
                 }
             }
         }
-        Map<Integer, List<Integer>> dependencyTree= new HashMap<>();
         int[] hardDependencies = new int[bricks.size()];
         for(Brick b: bricks){
             boolean allSame = true;
-            if(b.dependencies.size() > 0 ){
+            if(b.dependencies.size() > 0){
                 int id = b.dependencies.get(0);
                 for(int i = 1; i < b.dependencies.size(); i++){
                     if (b.dependencies.get(i) != id) {
@@ -109,8 +107,12 @@ public class Day22 extends DayTemplate {
             }
         }
         else{
+            Map<Integer, Set<Integer>> cache = new HashMap<>();
             for(int i = 0; i < bricks.size(); i++){
-                List<Integer> deadBricks = new ArrayList<>();
+                if(hardDependencies[i] == 0){
+                    continue;
+                }
+                Set<Integer> deadBricks = new HashSet<>();
                 deadBricks.add(i);
                 boolean noneAdded = false;
                 while(!noneAdded){
@@ -120,22 +122,27 @@ public class Day22 extends DayTemplate {
                             continue;
                         }
                         List<Integer> dependencies = bricks.get(j).dependencies;
-
                         boolean allDeps = dependencies.size() > 0;
-//                        System.out.println("brick " + j + " dependencies " + dependencies);
-                        for(Integer b: dependencies){
-                            if(!deadBricks.contains(b)){
-                                allDeps = false;
-                                break;
+                        if(allDeps){
+                            for(Integer b: dependencies){
+                                if(!deadBricks.contains(b)){
+                                    allDeps = false;
+                                    break;
+                                }
                             }
                         }
                         if(allDeps){
-                            deadBricks.add(j);
+                            if(cache.get(j) != null){
+                                deadBricks.addAll(cache.get(j));
+                            }
+                            else{
+                                deadBricks.add(j);
+                            }
                             noneAdded = false;
                         }
                     }
                 }
-//                System.out.println(deadBricks);
+                cache.put(i, deadBricks);
                 answer+= deadBricks.size() - 1;
             }
 
@@ -154,8 +161,7 @@ class Brick implements Comparable<Brick>{
     int dir;
     List<Integer> dependencies = new ArrayList<>();
 
-    public Brick(String line, int index){
-        id = index;
+    public Brick(String line){
         String[] ends = line.split("~");
         int x1 = Integer.parseInt(ends[0].split(",")[0]);
         int y1 = Integer.parseInt(ends[0].split(",")[1]);
@@ -191,13 +197,6 @@ class Brick implements Comparable<Brick>{
 
     }
 
-    public String toString(){
-        return "Brick with id: " + id + " at " + x+" " + y +" " + z + " facing " + dir + " with size " + size;
-    }
-
-    public void setZ(int newz){
-        z = newz;
-    }
     @Override
     public int compareTo(Brick o) {
         return o.z - z;
