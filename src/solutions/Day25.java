b828ffa818b41997bbeda432c716f01e8268cf12
abnew123 <shadowghosts31415@gmail.com>
2023-12-30 12:10:31 -0800

    2x speedup by only adding edges to long distances vertices

--- src/solutions/Day25.java
+++ src/solutions/Day25.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Day25 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
-        if(!part1){
+        if (!part1) {
             return "Merry Christmas!";
         }
         Map<String, List<String>> graph = new HashMap<>();
@@ -23,28 +23,25 @@ public class Day25 extends DayTemplate {
                 edges.add(new Edge(line[0], s));
             }
         }
-        List<String> verticesList = new ArrayList<>(graph.keySet());
         Map<Edge, Integer> frequencies = new HashMap<>();
-        for(Edge edge: edges){
-            frequencies.put(edge,0);
+        for (Edge edge : edges) {
+            frequencies.put(edge, 0);
         }
         getMostUsedEdges(graph, frequencies);
-        for(Edge e: frequencies.keySet()){
+        for (Edge e : frequencies.keySet()) {
             edges.get(edges.indexOf(e)).used = frequencies.get(e);
         }
         Collections.sort(edges);
-        for(int i = 0; i < 3; i++){
-            edges.remove(0);
-        }
+        edges.subList(0, 3).clear();
         Set<String> visited = new HashSet<>();
         Queue<String> queue = new LinkedList<>();
-
+        List<String> verticesList = new ArrayList<>(graph.keySet());
         queue.add(verticesList.get(0));
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             String s = queue.poll();
             visited.add(s);
-            for(String con: graph.get(s)){
-                if(!visited.contains(con) && edges.contains(new Edge(s, con))){
+            for (String con : graph.get(s)) {
+                if (!visited.contains(con) && edges.contains(new Edge(s, con))) {
                     queue.add(con);
                 }
             }
@@ -52,20 +49,22 @@ public class Day25 extends DayTemplate {
         return visited.size() * (graph.keySet().size() - visited.size()) + "";
     }
 
-    public void getMostUsedEdges(Map<String, List<String>> graph, Map<Edge,Integer> frequencies){
-        for(String vertex: graph.keySet()){
+    public void getMostUsedEdges(Map<String, List<String>> graph, Map<Edge, Integer> frequencies) {
+        for (String vertex : graph.keySet()) {
             Set<String> visited = new HashSet<>();
             visited.add(vertex);
-            Set<Route> routes = new HashSet<>();
+            List<Route> routes = new ArrayList<>();
             routes.add(new Route(vertex));
-            while(!routes.isEmpty()) {
-                Set<Route> routeTmp = new HashSet<>();
-                for(Route r: routes){
-                    for(Edge e: r.edges){
-                        frequencies.put(e, frequencies.get(e) + 1);
+            while (!routes.isEmpty()) {
+                List<Route> routeTmp = new ArrayList<>();
+                for (Route r : routes) {
+                    if (r.edges.size() > 10) {
+                        for (Edge e : r.edges) {
+                            frequencies.put(e, frequencies.get(e) + 1);
+                        }
                     }
-                    for(String con: graph.get(r.vertex)){
-                        if(!visited.contains(con)){
+                    for (String con : graph.get(r.vertex)) {
+                        if (!visited.contains(con)) {
                             Route tmp = new Route(con);
                             tmp.edges.addAll(r.edges);
                             tmp.edges.add(new Edge(r.vertex, con));
@@ -74,17 +73,17 @@ public class Day25 extends DayTemplate {
                         }
                     }
                 }
-               routes = routeTmp;
+                routes = routeTmp;
             }
         }
     }
 }
 
-class Route{
+class Route {
     String vertex;
     List<Edge> edges;
 
-    public Route(String start){
+    public Route(String start) {
         vertex = start;
         edges = new ArrayList<>();
     }
@@ -97,12 +96,8 @@ class Edge implements Comparable<Edge> {
     int used = 0;
 
     public Edge(String s, String e) {
-        start = (s.compareTo(e) > 0)?s: e;
-        end = (s.compareTo(e) > 0)?e: s;
-    }
-
-    public String toString() {
-        return start + " " + end + " " + used;
+        start = (s.compareTo(e) > 0) ? s : e;
+        end = (s.compareTo(e) > 0) ? e : s;
     }
 
     @Override
