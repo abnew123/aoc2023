f72d7a174a3b9c7355d2c5d82f0402a4defa65ea
abnew123 <shadowghosts31415@gmail.com>
2023-12-30 17:46:32 -0800

    switch Day21 from quadratic regression to direct computation, ~200x speedup

--- src/solutions/Day21.java
+++ src/solutions/Day21.java
@@ -28,74 +28,71 @@ public class Day21 extends DayTemplate {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (tmp.get(j)[i].equals("S")) {
-                    grid[i][j] = 9999;
                     x = i;
                     y = j;
+                    grid[i][j] = 0;
                 } else {
                     grid[i][j] = tmp.get(j)[i].equals("#") ? -1 : 9999;
                 }
             }
         }
-
-
-        Map<Coordinate, Integer> distances = new HashMap<>();
         List<Coordinate> reachablePoints = new ArrayList<>();
         reachablePoints.add(new Coordinate(x, y));
-        distances.put(new Coordinate(x, y), 0);
         int[] xs = new int[]{-1, 1, 0, 0};
         int[] ys = new int[]{0, 0, -1, 1};
         int index = 0;
-        List<Long> totals = new ArrayList<>();
-        List<Long> deltas = new ArrayList<>();
-        List<Long> deltaDeltas = new ArrayList<>();
-        long totalReached = 0;
-        while (index < (part1?64: 1000)) {
+        while (index < (part1?64: grid.length)) {
             index++;
             List<Coordinate> tmp2 = new ArrayList<>();
             for (Coordinate c : reachablePoints) {
                 for (int i = 0; i < 4; i++) {
                     int newx = c.x + xs[i];
                     int newy = c.y + ys[i];
-                    Coordinate candidate = new Coordinate(newx, newy);
-                    if (distances.get(candidate) == null) {
-                        if (grid[((newx % grid.length) + grid.length) % grid.length][((newy % grid.length) + grid.length) % grid.length] != -1) {
+                    if(newx >= 0 && newy >= 0 && newx < grid.length && newy < grid[0].length){
+                        if(grid[newx][newy] == 9999){
+                            Coordinate candidate = new Coordinate(newx, newy);
                             tmp2.add(candidate);
-                            distances.put(candidate, index);
+                            grid[newx][newy] = index;
                         }
                     }
                 }
             }
-            if (index % 2 == (part1?0:1)) {
-                totalReached += tmp2.size();
-                if (index % 262 == 65) {
-                    totals.add(totalReached);
-                    int currTotals= totals.size();
-                    if(currTotals > 1){
-                        deltas.add(totals.get(currTotals - 1) - totals.get(currTotals - 2));
-                    }
-                    int currDeltas= deltas.size();
-                    if(currDeltas > 1){
-                        deltaDeltas.add(deltas.get(currDeltas - 1) - deltas.get(currDeltas - 2));
-                    }
-                    if(deltaDeltas.size() > 1){
-                        break;
-                    }
-                }
-
-            }
             reachablePoints = tmp2;
         }
         if(part1){
-            answer = totalReached+1; //to account for the origin location
+            for(int i = 0; i < grid.length; i++){
+                for(int j = 0; j < grid[0].length; j++){
+                    if(grid[i][j] % 2 == 0 && grid[i][j] <= 64){
+                        answer++;
+                    }
+                }
+            }
         }
         if(!part1){
-            long neededLoopCount = 26501365 / 262 - 1;
-            long currentLoopCount = index / 262 - 1;
-            long deltaLoopCount = neededLoopCount - currentLoopCount;
-            long deltaLoopCountTriangular = (neededLoopCount * (neededLoopCount + 1))/2 - (currentLoopCount * (currentLoopCount + 1))/2;
-            long deltaDelta = deltaDeltas.get(deltaDeltas.size() - 1);
-            long initialDelta = deltas.get(0);
-            answer = (deltaDelta * deltaLoopCountTriangular + initialDelta * deltaLoopCount + totalReached );
+            long oddParity = 0;
+            long evenParity = 0;
+            long largeOddParity = 0;
+            long largeEvenParity = 0;
+            for(int i = 0; i < grid.length; i++){
+                for(int j = 0; j < grid[0].length; j++){
+                    if(grid[i][j] != -1 && grid[i][j] != 9999){
+                        if((i+j)%2 == 1){
+                            oddParity++;
+                            if(grid[i][j] > grid.length/2){
+                                largeOddParity++;
+                            }
+                        }
+                        else{
+                            evenParity++;
+                            if(grid[i][j] > grid.length/2){
+                                largeEvenParity++;
+                            }
+                        }
+                    }
+                }
+            }
+            long size = 26501365 / grid.length;
+            answer = ((size + 1) * (size + 1) * oddParity) + (size * size * evenParity) - ((size + 1) * largeOddParity) + (size * largeEvenParity);
         }
         return answer + "";
     }
