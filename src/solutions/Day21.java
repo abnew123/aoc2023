e4b75e40bffeaffeef6c2d5753793b3f6a812b07
abnew123 <shadowghosts31415@gmail.com>
2024-02-29 00:50:09 -0500

    refactor Day 21

--- src/solutions/Day21.java
+++ src/solutions/Day21.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
 
 public class Day21 implements DayTemplate {
 
+    int[][] grid;
+
     /**
      * Main solving method.
      *
@@ -19,27 +21,8 @@ public class Day21 implements DayTemplate {
      */
     public String solve(boolean part1, Scanner in) {
         long answer = 0;
-        List<String[]> tmp = new ArrayList<>();
-        while (in.hasNext()) {
-            String line = in.nextLine();
-            tmp.add(line.split(""));
-        }
-        int x = 0;
-        int y = 0;
-        int[][] grid = new int[tmp.get(0).length][tmp.size()];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (tmp.get(j)[i].equals("S")) {
-                    x = i;
-                    y = j;
-                    grid[i][j] = 0;
-                } else {
-                    grid[i][j] = tmp.get(j)[i].equals("#") ? -1 : 9999;
-                }
-            }
-        }
         List<Coordinate> reachablePoints = new ArrayList<>();
-        reachablePoints.add(new Coordinate(x, y));
+        reachablePoints.add(buildGridAndGetStart(in));
         int[] xs = new int[]{-1, 1, 0, 0};
         int[] ys = new int[]{0, 0, -1, 1};
         int index = 0;
@@ -60,39 +43,72 @@ public class Day21 implements DayTemplate {
             reachablePoints = tmp2;
         }
         if (part1) {
-            for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid[0].length; j++) {
-                    if (grid[i][j] % 2 == 0 && grid[i][j] <= 64) {
-                        answer++;
-                    }
+            answer = solvePart1();
+        }
+        if (!part1) {
+            answer = solvePart2();
+        }
+        return answer + "";
+    }
+
+    private Coordinate buildGridAndGetStart(Scanner in){
+        List<String[]> tmp = new ArrayList<>();
+        while (in.hasNext()) {
+            String line = in.nextLine();
+            tmp.add(line.split(""));
+        }
+        int x = 0;
+        int y = 0;
+        grid = new int[tmp.get(0).length][tmp.size()];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (tmp.get(j)[i].equals("S")) {
+                    x = i;
+                    y = j;
+                    grid[i][j] = 0;
+                } else {
+                    grid[i][j] = tmp.get(j)[i].equals("#") ? -1 : 9999;
                 }
             }
         }
-        if (!part1) {
-            long oddParity = 0;
-            long evenParity = 0;
-            long largeOddParity = 0;
-            long largeEvenParity = 0;
-            for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid[0].length; j++) {
-                    if (grid[i][j] != -1 && grid[i][j] != 9999) {
-                        if ((i + j) % 2 == 1) {
-                            oddParity++;
-                            if (grid[i][j] > grid.length / 2) {
-                                largeOddParity++;
-                            }
-                        } else {
-                            evenParity++;
-                            if (grid[i][j] > grid.length / 2) {
-                                largeEvenParity++;
-                            }
+        return new Coordinate(x,y);
+    }
+
+    private long solvePart1(){
+        long answer = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] % 2 == 0 && grid[i][j] <= 64) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    private long solvePart2(){
+        long oddParity = 0;
+        long evenParity = 0;
+        long largeOddParity = 0;
+        long largeEvenParity = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != -1 && grid[i][j] != 9999) {
+                    if ((i + j) % 2 == 1) {
+                        oddParity++;
+                        if (grid[i][j] > grid.length / 2) {
+                            largeOddParity++;
+                        }
+                    } else {
+                        evenParity++;
+                        if (grid[i][j] > grid.length / 2) {
+                            largeEvenParity++;
                         }
                     }
                 }
             }
-            long size = 26501365 / grid.length;
-            answer = ((size + 1) * (size + 1) * oddParity) + (size * size * evenParity) - ((size + 1) * largeOddParity) + (size * largeEvenParity);
         }
-        return answer + "";
+        long size = 26501365 / grid.length;
+        return ((size + 1) * (size + 1) * oddParity) + (size * size * evenParity) - ((size + 1) * largeOddParity) + (size * largeEvenParity);
     }
 }
