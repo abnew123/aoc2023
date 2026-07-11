acf52216373289c3d884909ede9db97242add1e7
abnew123 <shadowghosts31415@gmail.com>
2026-07-11 11:34:25 -0400

    Recover lens slots from global order

--- src/solutions/Day15Golfed.java
+++ src/solutions/Day15Golfed.java
@@ -1,4 +1,4 @@
 class O{
-String s(boolean p,String[]I){var a=String.join("",I).split(",");long r=0;if(p){for(var x:a)r+=h(x);return r+"";}var b=new java.util.LinkedHashMap[256];for(int i=0;i<256;i++)b[i]=new java.util.LinkedHashMap();for(var x:a){int e=x.indexOf(61),q=e<0?x.length()-1:e;var l=x.substring(0,q);var m=b[h(l)];if(e<0)m.remove(l);else m.put(l,Integer.valueOf(x.substring(e+1)));}for(int i=0;i<256;i++){int j=0;for(var v:b[i].values())r+=(i+1L)*++j*(int)v;}return r+"";}
+String s(boolean p,String[]I){var a=String.join("",I).split(",");long r=0;if(p){for(var x:a)r+=h(x);return r+"";}var m=new java.util.LinkedHashMap();for(var x:a){int e=x.indexOf(61),q=e<0?x.length()-1:e;var l=x.substring(0,q);if(e<0)m.remove(l);else m.put(l,Integer.valueOf(x.substring(e+1)));}var z=new int[256];for(var e:m.keySet()){int b=h((String)e);r+=(b+1L)*++z[b]*(int)m.get(e);}return r+"";}
 int h(String s){int v=0;for(char c:s.toCharArray())v=(v+c)*17%256;return v;}
 }
