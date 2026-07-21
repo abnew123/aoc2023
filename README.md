130b4bec040777c030c4006e3ab5105537edc609
abnew123 <shadowghosts31415@gmail.com>
2026-07-21 18:08:16 -0400

    Golf day 23 for 63 characters

--- README.md
+++ README.md
@@ -145,9 +145,9 @@ The table below counts non-whitespace characters in each normal solution file an
 | 20 | [Source](src/solutions/Day20.java) | [Golfed](src/solutions/Day20Golfed.java) | 5,148 | 1,024 |
 | 21 | [Source](src/solutions/Day21.java) | [Golfed](src/solutions/Day21Golfed.java) | 2,022 | 456 |
 | 22 | [Source](src/solutions/Day22.java) | [Golfed](src/solutions/Day22Golfed.java) | 4,199 | 599 |
-| 23 | [Source](src/solutions/Day23.java) | [Golfed](src/solutions/Day23Golfed.java) | 8,580 | 741 |
+| 23 | [Source](src/solutions/Day23.java) | [Golfed](src/solutions/Day23Golfed.java) | 8,580 | 678 |
 | 24 | [Source](src/solutions/Day24.java) | [Golfed](src/solutions/Day24Golfed.java) | 4,574 | 1,063 |
 | 25 | [Source](src/solutions/Day25.java) | [Golfed](src/solutions/Day25Golfed.java) | 2,858 | 489 |
-| Total |  |  | 75,232 | 11,602 |
+| Total |  |  | 75,232 | 11,539 |
 
 <!-- CHAR COUNTS END -->
--- src/solutions/Day23Golfed.java
+++ src/solutions/Day23Golfed.java
@@ -1,12 +1,12 @@
 class W{
-int g[],L;String G;boolean P;
-String s(boolean p,String[]Z){P=p;G=String.join("\n",Z);int w=G.indexOf(10)+1,N=0,h[]=new int[L=G.length()],v,D[]={1,w,-1,-w};
+int g[],L;byte[]G;boolean P;
+String s(boolean p,String[]Z){P=p;G=String.join("\n",Z).getBytes();int N=0,h[]=new int[L=G.length],w=(L+1)/Z.length,v,D[]={1,w,-1,-w};
 for(v=L;v-->0;)if(o(v)){w=0;for(int d:D)if(o(v+d))w++;if(w!=2)h[v]=N+=5;}
-g=new int[N];
-for(v=L;v-->0;)if(h[v]>0)for(N=4;N-->0;)if(a(G.charAt(v),N))for(int f=h[v]-5,x=v,t=N,s=0;;){if(!o(x+=D[t]))break;s++;if(h[x]>0){g[f+N]=h[x]/5*L-L+s;break;}w=4;while(w-->0&&!(a(G.charAt(x),w)&o(x+D[w])&w!=(t^2)));if(w<0)break;t=w;}
-return""+d(h[G.indexOf(46)]-5);
+g=new int[N+5];
+for(v=L;v-->0;)if(h[v]>0)for(int e=4;e-->0;)if(a(G[v],e))for(int x=v,t=e,s=0;t>=0&&o(x+=D[t]);t=w){s++;if(h[x]>0){g[h[v]-~e]=h[x]*L+s;break;}w=4;while(w-->0&&!(a(G[x],w)&o(x+D[w])&w!=(t^2)));}
+return""+d(N);
 }
-int d(int a){if(a<1)return 0;g[a+4]=1;int b=-L;for(int z=4,q;z-->0;)if((q=g[a+z])>0&g[q/L*5+4]<1)b=Math.max(b,q%L+d(q/L*5));g[a+4]=0;return b;}
-boolean a(char c,int d){return!P|c<47|c==">v<^".charAt(d);}
-boolean o(int v){return v>=0&v<L&&G.charAt(v)>35;}
+int d(int a){if(a<6)return 0;g[a]=1;int b=-L;for(int z=5,q;z-->1;)if((q=g[a+z])>0&g[q/L]<1)b=Math.max(b,q%L+d(q/L));g[a]=0;return b;}
+boolean a(byte c,int d){return!P|c<47|c==">v<^".charAt(d);}
+boolean o(int v){return v>=0&v<L&&G[v]>35;}
 }
